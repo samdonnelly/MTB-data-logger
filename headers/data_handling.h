ac7c733f0e20000f6654ff5689becb2bc87746c8
Sam Donnelly <samueldonnelly11@gmail.com>
2023-05-24 15:10:28 -0600

    Added a data handling file that will deal with all the data reading and writing. Renamed the directories files.

---
+++ headers/data_handling.h
@@ -0,0 +1,39 @@
+/**
+ * @file data_handling.h
+ * 
+ * @author Sam Donnelly (samueldonnelly11@gmail.com)
+ * 
+ * @brief MTB DL data handling header 
+ * 
+ * @version 0.1
+ * @date 2023-05-24
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _DATA_HANDLING_H_ 
+#define _DATA_HANDLING_H_ 
+
+//=======================================================================================
+// Includes 
+
+#include "includes_app.h"
+#include "includes_drivers.h"
+
+//=======================================================================================
+
+
+//=======================================================================================
+// Macros 
+
+#define MTBDL_MAX_DATA_STR_LEN 50        // Max string length containing data 
+
+//=======================================================================================
+
+
+//=======================================================================================
+// Function prototypes 
+//=======================================================================================
+
+#endif   // _DATA_HANDLING_H_ 
